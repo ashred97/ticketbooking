6caf73ac2c788807efa91d5e7ddcbbd4bd67fc92
Aashish Reddy <70836394+ashred97@users.noreply.github.com>
2022-04-25 13:10:07 +0530

    added user and admin pages

---
+++ app/controllers/concerns/user_login_form_controller.rb
@@ -0,0 +1,4 @@
+class User_login_formController < ApplicationController
+    def user_login_form
+    end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,13 @@
 Rails.application.routes.draw do
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-
   # Defines the root path route ("/")
   # root "articles#index"
+
+get "/" , to: "main#index"
+get "sign_up" , to: "registration#new"
+# get "/" , to: "main#user_login_form"
+get "/user_login_form", to: "main#user_login_form"
+get "/admin_login_form", to: "main#admin_login_form"
+get "/user_home", to: "main#user_home"
+get "/admin_home", to: "main#admin_home"
 end
